4dbc0076deec4c1feac0186d74e5ff9f2aefff3f
Araceliruano <101236610+Araceliruano@users.noreply.github.com>
2022-04-28 20:25:09 -0500

    Update RuanoAraceli-Compara.cpp

--- RuanoAraceli-Compara.cpp
+++ RuanoAraceli-Compara.cpp
@@ -5,19 +5,19 @@ using namespace std;
  
 int main()
 {
-int N1;
-int N2;
+   int N1;
+   int N2;
 cout << "ingrese primer numero:";
 cin >> N1;
 cout << "ingrese segundo numero:";
 cin >> N2;
 if (N1>N2)
 {
-  cout << "El mayor es:" <<N1;
+       cout << "El mayor es:" <<N1;
 }
 if (N2>N1)
 {
-  cout << "El mayor es:" <<N2;
+        cout << "El mayor es:" <<N2;
 }
 if (N1==N2)
 {
