cf8fc2ab3646c5ed9c351635cc78ae496000cff5
Araceliruano <101236610+Araceliruano@users.noreply.github.com>
2022-05-20 17:05:15 -0500

    Update RuanoAraceli-PuntoVenta.cpp

--- RuanoAraceli-PuntoVenta.cpp
+++ RuanoAraceli-PuntoVenta.cpp
@@ -1,58 +1,52 @@
+//=============================================================================
+//==>Nombre del programa:compara
+//==>Autor:Araceli Ruano
+//==>Fecha de elaboracion:2022-04-30
+//==>Fecha de actualizacion:2022-05-20
+//============================================================================
 #include<iostream>
 using namespace std;
 
 int main() {
-	// Primero declaramos las variables que vamos a utilizar//
-	float cantidadcompra;
-	int contador;
-	float desc;
-	float iva;
-	float subtotal;
-	float subtotalcompra;
-	float totalcompra;
-	float totaldesc;
-	float totaliva;
-	int totalproductos;
-	float valordesc;
-	float valoriva;
-	float valorproducto;
-	int x;
-	cout << "Ingresar la cantidad de productos a comprar" << endl; //Le pedimos al usuario ingresar la cantidad total de productos//
-	cin >> x;
-	contador = 0;
-	totaldesc = 0;
-	totalproductos = 0;
-	totalcompra = 0;
-	totaliva = 0;
-	// Llamamos un bucle para ingresar los productos y sus valores//
-	while (contador<x) {
-		cout << "Ingrese el valor del producto" << endl;
-		cin >> valorproducto;
-		cout << "Ingrese la cantidad de productos" << endl;
-		cin >> cantidadcompra;
-		subtotal = valorproducto*cantidadcompra;
-		cout << "Ingrese el descuento" << endl;
-		cin >> desc;
-		valordesc = subtotal*desc/100;
-		subtotalcompra = subtotal-valordesc;
-		cout << "Ingrese el IVA" << endl;
-		cin >> iva;
-		valoriva = subtotal*iva/100;
-		subtotalcompra = subtotal-valoriva;
-		cout << "El descuento es:" << valordesc << endl;
-		cout << "El subtotal es:" << subtotal << endl;
-		cout << "El iva es:" << valoriva << endl;
-		totalproductos = totalproductos+cantidadcompra;
-		totaldesc = totaldesc+valordesc;
-		totalcompra = totalcompra+subtotalcompra;
-		totaliva = totaliva+valoriva;
-		contador = contador+1;
-	}
-	//Estos seran los valores finales a mostrar//
-	cout << "La cantidad de productos comprados es:" << totalproductos << endl;
-	cout << "El total de descuento en la compra es:" << totaldesc << endl;
-	cout << "El total de iva de la compra es:" << valoriva << endl;
-	cout << "El total de la compra es:" << totalcompra << endl;
-	return 0;
+	
+	int AR_c=0,AR_a=0;
+	float AR_desc;
+	float AR_iva;
+	int AR_l;
+	float AR_Pdesc;
+	float AR_Piva;
+	float AR_tb;
+	float AR_tn;
+	float AR_x;
+	cout<<"Ingresa la cantidad de productos:";
+	cin >> AR_l;
+	cout<<"Ingresar el porcentaje de iva:";
+	cin>> AR_Piva;
+	cout<<"ingresar el porcentaje de descuento:";
+	cin>>AR_Pdesc;
+	do{
+	cout<<"Ingresar el valor del producto"<<AR_c+1;
+	cin>>AR_x;
+	AR_c=AR_c+1;
+	AR_a=AR_a+AR_x;
+	AR_tb=AR_a;
+    AR_iva = AR_tb*AR_Piva/100;
+	AR_desc = AR_tb*AR_Pdesc/100;
+	AR_tn = AR_tb+AR_iva-AR_desc;
+	
+    }while(AR_c<AR_l);
+    cout<<"La suma total es:"<<AR_a<<endl;
+    cout<<"El iva total es:"<<AR_iva<<endl;
+    cout<<"El descuento es"<<AR_desc<<endl;
+    cout<<"El total a pagar es:"<<AR_tn<<endl;
+    cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: PuntoVenta"<<endl;
+cout<<"//==> Archivo : RuanoAraceli-PuntoVenta.cpp"<<endl;
+cout<<"//==>Autor: Araceli Ruano"<<endl;
+cout<<"//==>Fecha de elaboracion: 2022-04-30"<<endl;
+cout<<"//==>Fecha ultima actualizacion: 2022-05-20"<<endl;
+cout<<"//================================================="<<endl;
+	return 0;	
+
 }
 
