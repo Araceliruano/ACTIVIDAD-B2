7f8c32478d91fbcb76012155d3f6cf4344ed0b55
Araceliruano <101236610+Araceliruano@users.noreply.github.com>
2022-05-20 17:18:21 -0500

    Update RuanoAraceli-SumaN.cpp

--- RuanoAraceli-SumaN.cpp
+++ RuanoAraceli-SumaN.cpp
@@ -1,24 +1,31 @@
+//=============================================================================
+//==>Nombre del programa:SumaN
+//==>Autor:Araceli Ruano
+//==>Fecha de elaboracion:2022-04-28
+//==>Fecha de actualizacion:2022-05-20
+//============================================================================
 #include<iostream>
 using namespace std;
 
 int main() {
-	// declaramos las variables//
-	int n;
-	int suma;
-	int total;
-	int x;
-	// solicitamos la cantidad de numeros//
-	cout << "ingresa el total de numeros" << endl;
-	cin >> total;
-	x = 1;
-	suma = 0;
-	//utilizamos un bucle para ingresar los valores//
-	while (x<=total) {
-		cout << "ingresa el numero" << x << endl;
-		cin >> n;
-		suma = suma+n;
-		x = x+1;
+	int AR_c=0,AR_N;
+	float AR_s=0,AR_X;
+	cout<<"Ingrese la cantidad de valores a sumar: ";
+	cin>>AR_N;
+	do{
+		cout<<"Ingrese el elemento "<<AR_c+1;
+		cin>>AR_X;
+		AR_c=AR_c+1;
+		AR_s=AR_s+AR_X;
 	}
-	cout << "la suma de los numeros es:" << suma << endl; // mostramos el resultado de la suma de los valores ingresados//
+	while(AR_c<AR_N);
+	cout<<"La suma de elementos es:"<<AR_s<<endl;
+	cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: SumaN"<<endl;
+cout<<"//==> Archivo : RuanoAraceli-SumaN.cpp"<<endl;
+cout<<"//==>Autor: Araceli Ruano"<<endl;
+cout<<"//==>Fecha de elaboracion: 2022-04-28"<<endl;
+cout<<"//==>Fecha ultima actualizacion: 2022-05-20"<<endl;
+cout<<"//================================================="<<endl;
 	return 0;
 }
