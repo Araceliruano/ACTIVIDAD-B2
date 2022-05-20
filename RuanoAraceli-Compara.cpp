9f7b354b5cdc133238d33c2d3e85f2b00f187fc3
Araceliruano <101236610+Araceliruano@users.noreply.github.com>
2022-05-20 16:57:50 -0500

    Update RuanoAraceli-Compara.cpp

--- RuanoAraceli-Compara.cpp
+++ RuanoAraceli-Compara.cpp
@@ -1,28 +1,34 @@
-#include <conio.h>
-#include <stdio.h>
-#include <iostream>
+//=============================================================================
+//==>Nombre del programa:compara
+//==>Autor:Araceli Ruano
+//==>Fecha de elaboracion:2022-04-28
+//==>Fecha de actualizacion:2022-05-20
+//============================================================================
+#include<iostream>
 using namespace std;
-int main()
-{
-// declaramos las variables//
-   int N1;
-   int N2;
-//ingresamos los numeros//
-cout << "ingrese primer numero:";
-cin >> N1;
-cout << "ingrese segundo numero:";
-cin >> N2;
-// declaramos las condiciones//
-if (N1>N2)//si N1 es mayor que N2 entonces//
-{
-       cout << "El mayor es:" <<N1;
-}
-if (N2>N1)//si N2 es mayor que N1 entonces//
-{
-        cout << "El mayor es:" <<N2;
-}
-if (N1==N2)//si N1 es igual a N2 entonces//
-{
-	cout << "Son iguales";
-}
+
+int main() {
+	float AR_N1;
+	float AR_N2;
+	cout << "Ingrese el valor de AR_N1:" << endl;
+	cin>>AR_N1;
+	cout << "Ingrese el valor de AR_N2:"<< endl;
+	cin>>AR_N2;
+	if (AR_N1==AR_N2) {
+		cout<<"El valor de RA_N1:"<< "Es igual a AR_N2:" << endl;
+	} else {
+		if (AR_N1<AR_N2) {
+			cout<<"El valor de AR_N1:" << "Es menor que AR_N2:"<< endl;
+		} else {
+			cout<<"El valor de AR_N2:"<< "Es menor que AR_N1:" << endl;
+		}
+	}
+	cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: Compara"<<endl;
+cout<<"//==> Archivo : RuanoAraceli-Compara.cpp"<<endl;
+cout<<"//==>Autor: Araceli Ruano"<<endl;
+cout<<"//==>Fecha de elaboracion: 2022-04-28"<<endl;
+cout<<"//==>Fecha ultima actualizacion: 2022-05-20"<<endl;
+cout<<"//================================================="<<endl;
+	return 0;
 }
